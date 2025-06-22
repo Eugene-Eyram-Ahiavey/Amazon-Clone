d02f47ba2e8752aa61b4325d13b3d3d4dee30067
Eugene Eyram Ahiavey <phostechservices@gmail.com>
2025-06-22 18:43:14 +0000

    load products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,12 @@
 // import {cart, addToCart, calculateCartQuantity} from "../data/cart.js";
 import { cart } from "../data/new-cart-class.js";
-import { products } from "../data/products.js"
+import { products, loadProducts } from "../data/products.js"
 // import { formatCurrency } from "./utils/money.js";
 
 
+loadProducts(renderProductsGrid);
+
+function renderProductsGrid(){
 
 let productsHTML = '';
 
@@ -112,5 +115,5 @@ addedTimeout();
 
   });
 });
-
+}
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentOrderSummary } from "./checkout/paymentSummary.js";
 import '../data/cart-class.js';
-import '../data/car.js';
-import "../data/backend-practice.js";
+import { loadProducts } from "../data/products.js";
+// import '../data/car.js';
+// import "../data/backend-practice.js";
 
-
-
-
+loadProducts(() => {
 renderOrderSummary();
 renderPaymentOrderSummary();
+})
+
